770bf0fadfde5f0c6a56ec02d74c197bf5163b9d
Jordanoo <jordanhernandez86@gmail.com>
2022-02-27 10:21:41 +0100

    add modal & details

---
+++ src/components/movies/MovieModal.tsx
@@ -0,0 +1,91 @@
+import { useContext } from "react";
+
+import getMoviesById from "../../service/getMovieById";
+import MovieModalDetail from "./MovieModalDetail";
+import {
+  DispatchContext,
+  MovieDetailsWithId,
+  StateContext,
+  StateSearch,
+  UserActionType,
+  UserDispatch,
+} from "../../store";
+
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { IconButton, Tooltip } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+const style = {
+  modal: {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "black",
+    boxShadow: 24,
+    overflow: "scroll",
+  },
+  icon: {
+    color: "white",
+  },
+  cardContainer: {
+    borderRadius: 0,
+    cursor: "auto",
+    alignItems: "stretch",
+    height: "100%",
+    bgcolor: "black",
+    color: "white",
+  },
+};
+
+interface MovieModalProps {
+  imdbID: string;
+}
+
+export default function MovieModal(props: MovieModalProps) {
+  const { imdbID } = props;
+  const state = useContext(StateContext);
+  const dispatch = useContext(DispatchContext) as UserDispatch;
+  const { movie, isOpen } = state as StateSearch;
+
+  const handleClickClose = () => {
+    dispatch({ type: UserActionType.SET_MODAL_CLOSE });
+  };
+
+  const handleClickOpen = async () => {
+    try {
+      await getMoviesById({ imdbID }, dispatch);
+      dispatch({ type: UserActionType.SET_MODAL_OPEN });
+      console.log(movie, "movie");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box>
+      <Tooltip title="More info" arrow>
+        <IconButton onClick={handleClickOpen} className="icon" sx={style.icon}>
+          <InfoIcon />
+        </IconButton>
+      </Tooltip>
+      <Modal
+        sx={{
+          overflow: "scroll",
+        }}
+        keepMounted
+        open={isOpen}
+        onClose={handleClickClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style.modal}>
+          <MovieModalDetail {...(movie as unknown as MovieDetailsWithId)} />
+        </Box>
+      </Modal>
+    </Box>
+  );
+}
---
+++ src/components/movies/MovieModalDetail.tsx
@@ -0,0 +1,127 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+
+import {
+  DispatchContext,
+  MovieDetailsWithId,
+  StateContext,
+  StateSearch,
+  UserActionType,
+  UserDispatch,
+} from "../../store";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+
+const style = {
+  modal: {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "black",
+    boxShadow: 24,
+    overflow: "scroll",
+  },
+  icon: {
+    color: "white",
+  },
+  cardContainer: {
+    borderRadius: 0,
+    cursor: "auto",
+    alignItems: "stretch",
+    height: "100%",
+    bgcolor: "black",
+    color: "white",
+  },
+};
+
+interface MovieModalProps {
+  imdbID: string;
+}
+
+export default function MovieModalDetail({
+  Title,
+  Actors,
+  Poster,
+  Rated,
+  Genre,
+  Plot,
+  Released,
+  Writer,
+}: MovieDetailsWithId) {
+  const [truncate, setTruncate] = useState(true);
+  const clickHandle = () => setTruncate(!truncate);
+  return (
+    <>
+      <Card sx={style.cardContainer}>
+        <CardMedia
+          component="img"
+          image={Poster}
+          alt={Title}
+          sx={{ height: "100%" }}
+        />
+        <Box
+          sx={{
+            bgcolor: "black",
+            py: 2,
+            px: 1,
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Tooltip title="add to favorite" arrow>
+            <IconButton className="icon" sx={style.icon}>
+              <FavoriteIcon />
+            </IconButton>
+          </Tooltip>
+          <Button
+            sx={{ bgcolor: "white", width: 100, ml: 10, color: "black" }}
+            size="medium"
+            startIcon={<PlayArrowIcon />}
+          >
+            Play
+          </Button>
+        </Box>
+        <Box sx={{ px: 2, pb: 1 }}>
+          <Box display="flex" justifyContent="space-between" pb={1}>
+            <Typography variant="caption" gutterBottom>
+              Release date: {Released}
+            </Typography>
+            <Typography variant="caption" gutterBottom>
+              Rating: {Rated}
+            </Typography>
+          </Box>
+          <CardActionArea
+            sx={{ transition: "transform 0.2s" }}
+            disableRipple
+            onClick={clickHandle}
+          >
+            <Typography noWrap={truncate} variant="body2">
+              {Plot}
+            </Typography>
+          </CardActionArea>
+        </Box>
+        <Box sx={{ p: 2 }}>
+          <Typography variant="body2">Actors: {Actors}</Typography>
+          <Typography gutterBottom variant="body2">
+            Writer: {Writer}
+          </Typography>
+          <Typography gutterBottom variant="caption">
+            Genre: {Genre}
+          </Typography>
+        </Box>
+      </Card>
+    </>
+  );
+}
