d538d60234a3a832de0a2cc74a15b0421d15fa2b
Jordanoo <jordanhernandez86@gmail.com>
2022-02-27 10:15:55 +0100

    add reducer

---
+++ src/store/reducer.ts
@@ -0,0 +1,53 @@
+import { StateSearch } from "./index";
+import { UserAction, UserActionType } from "../store/actions.d";
+
+export function movieReducer(state: StateSearch, action: UserAction) {
+  switch (action.type) {
+    case UserActionType.SEARCH:
+      return {
+        ...state,
+        isLoading: true,
+        error: "",
+      };
+    case UserActionType.SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: "",
+      };
+    case UserActionType.ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: "movie doesn't exist",
+      };
+    case UserActionType.SET_MODAL_OPEN:
+      return {
+        ...state,
+        isOpen: true,
+      };
+    case UserActionType.SET_MODAL_CLOSE:
+      return {
+        ...state,
+        isOpen: false,
+      };
+    case UserActionType.SET_MOVIE_DETAIL:
+      return {
+        ...state,
+        movies: action.payload,
+      };
+    case UserActionType.SET_MOVIE_DETAIL_BY_ID:
+      return {
+        ...state,
+        movie: action.payload,
+      };
+    case UserActionType.FIELD:
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    default:
+      break;
+  }
+  return state;
+}
