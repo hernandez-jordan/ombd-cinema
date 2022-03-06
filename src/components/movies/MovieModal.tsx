6152bc7594183f6c63cd088bbb61d7ba292e4afc
Jordanoo <jordanhernandez86@gmail.com>
2022-03-06 01:50:27 +0100

    fix css styling

--- src/components/movies/MovieModal.tsx
+++ src/components/movies/MovieModal.tsx
@@ -20,13 +20,10 @@ const style = {
     overflow: "scroll",
   },
   container: {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
     bgcolor: "'background.paper'",
     boxShadow: 24,
     overflow: "scroll",
+    m: "auto",
     width: { xs: "100%", sm: 400 },
   },
 };
