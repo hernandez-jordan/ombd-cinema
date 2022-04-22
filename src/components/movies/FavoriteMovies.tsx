582de9859e2da1ab3369036dccbd4f98ef8c3c64
Jordanoo <jordanhernandez86@gmail.com>
2022-04-22 14:03:19 +0200

    add fav movie main component

---
+++ src/components/movies/FavoriteMovies.tsx
@@ -0,0 +1,44 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useContext } from "react";
+import { StateContext, StateSearch } from "../../store";
+import SearchBar from "../searchbar/SearchBar";
+import Loader from "../loader/Loader";
+import FavoritesCard from "./FavoritesCard";
+
+const style = {
+  gridContainer: {
+    flexWrap: "nowrap",
+    display: "-webkit-inline-box",
+  },
+};
+
+export default function Movies() {
+  const state = useContext(StateContext);
+  const { isLoading } = state as StateSearch;
+
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection={{ xs: "column-reverse", sm: "row" }}
+        alignItems={{ md: "center" }}
+        justifyContent="space-between"
+        pb={2}
+        pt={{ xs: 2 }}
+      >
+        <Typography
+          sx={{ typography: { md: "h4", xs: "h5" } }}
+          align="left"
+          py={{ xs: 1, sm: 2, md: 3 }}
+          pt={{ xs: 2 }}
+        >
+          Favorite Movie List
+        </Typography>
+      </Box>
+      <Grid container spacing={3} className="row" sx={style.gridContainer}>
+        {isLoading ? <Loader /> : <FavoritesCard />}
+      </Grid>
+    </>
+  );
+}
