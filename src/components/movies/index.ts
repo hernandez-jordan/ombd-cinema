60c06b65d6335af128e89ef341231c177864dc15
Jordanoo <jordanhernandez86@gmail.com>
2022-04-22 13:55:27 +0200

    index files added

--- src/components/movies/index.ts
+++ src/components/movies/index.ts
@@ -1,2 +1,6 @@
 export * from "./Movies";
 export * from "./MovieModal";
+export * from "./MovieCard";
+export * from "./MovieModalDetail";
+export * from "./FavoritesCard";
+export * from "./FavoriteMovies";
